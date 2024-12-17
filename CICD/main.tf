9d8496bf9a09196ec81e573d642f10addfea4f35
hemanthdivvela <divvelahemanth7@gmail.com>
2024-12-17 10:37:21 +0530

    update

--- CICD/main.tf
+++ CICD/main.tf
@@ -61,7 +61,7 @@ module "nexus" {
 
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
-  version = "~> 2.0"
+  version = "~> 3.1.0"
 
   zone_name = var.zone_name
 
