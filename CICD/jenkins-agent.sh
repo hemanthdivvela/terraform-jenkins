9a9865ba315dafe25acef74c7172886c54e0ce12
hemanthdivvela <divvelahemanth7@gmail.com>
2024-12-18 13:29:56 +0530

    jenkins-agent

--- CICD/jenkins-agent.sh
+++ CICD/jenkins-agent.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 yum install fontconfig java-17-openjdk -y
-yum install -y yum-utils shadow-utils
-yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+yum install -y yum-utils 
+# yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 yum -y install terraform
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
